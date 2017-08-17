e9aa973c254e84473ed296d010caba278f0a64fb
Lucas Thompson <dev@lucas.im>
2017-08-17 11:58:36 +0100

    Change stackblitz url

--- presentation/index.js
+++ presentation/index.js
@@ -122,7 +122,7 @@ const uglyDucklingSlide = (
 const liveCodingSlide = (
   <Slide>
     <Heading size={6}>Live Coding</Heading>
-    <Iframe url="https://stackblitz.com/edit/piano-roll-component?embed=1&file=index.js&hideExplorer=1&hideNavigation=1"
+    <Iframe url="https://stackblitz.com/edit/melody-transcriber?embed=1&file=Transcriber.ts&hideExplorer=1&hideNavigation=1"
       display="block"
       position="relative"
       height="600px"
